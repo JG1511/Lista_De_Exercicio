cc4afee04fc1c1556840bc42419f984677cb36c4
JG1511 <joaotsi2024@gmail.com>
2024-07-11 07:47:28 -0300

    02

--- Lista/questao.py
+++ Lista/questao.py
@@ -1,6 +1,10 @@
 # Questão 1
 
-for i in range(1, 100 +1, 1):
-    print(i, end = (","))
+# for i in range(1, 100 +1, 1):
+#     print(i, end = (","))
 
+# Questão 2
+
+for i in range(100, 1 -1 , -1):
+    print(i, end = (","))
 
