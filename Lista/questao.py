977f6a3e402dfc39aed20553e5bb802a786e84fb
JG1511 <joaotsi2024@gmail.com>
2024-07-11 07:52:06 -0300

    01

--- Lista/questao.py
+++ Lista/questao.py
@@ -1,10 +1,8 @@
-# Questão 1
+# Questão 01
 
-# for i in range(1, 100 +1, 1):
-#     print(i, end = (","))
-
-# Questão 2
-
-for i in range(100, 1 -1 , -1):
+for i in range(1, 100 +1, 1):
     print(i, end = (","))
+
+
+
 
