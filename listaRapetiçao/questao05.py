2a4e1d134faec8a2841d5d61ea3b8e9da3e8c572
JG1511 <joaotsi2024@gmail>
2024-07-12 14:58:02 -0300

    questao05

---
+++ listaRapetiçao/questao05.py
@@ -0,0 +1,14 @@
+# Escreva um programa que leia dois valores inteiros e escreva como saída a diferença
+# entre o maior valor e o menor valor.
+
+valor = int(input("Digite o primeiro valor: "))
+valor2= int(input("Digite o segundo valor: "))
+
+if valor < valor2:
+    valor_sub = valor2 - valor
+    print("A diferença entre o maior valor e o menor: ", valor_sub)
+elif valor2 < valor:
+     valor_sub = valor - valor2
+     print("A diferença entre o maior valor e o menor: ",  valor_sub)
+    
+   
