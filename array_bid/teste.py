7a54631f92e1d904d4c3bc08995d9284f14e95c2
JG1511 <joaotsi2024@gmail>
2024-08-13 21:33:27 -0300

    teste2

--- array_bid/teste.py
+++ array_bid/teste.py
@@ -33,17 +33,40 @@
 
 #Questão 06
 
-lista_numero = []
-lista_nome = []
+# lista_numero = []
+# lista_nome = []
 
-for i in range(6):
-    valor = int(input("Digite os valores: "))
-    lista_numero += [valor]
-for i in range(6):
-    letra = input("Digite letras: ")
-    lista_nome += [letra]
+# for i in range(6):
+#     valor = int(input("Digite os valores: "))
+#     lista_numero += [valor]
+# for i in range(6):
+#     letra = input("Digite letras: ")
+#     lista_nome += [letra]
 
-for i in range (1,6,2):
-    lista_numero[i] = lista_nome [i]
+# for i in range (1,6,2):
+#     lista_numero[i] = lista_nome [i]
 
-print(lista_numero)
+# print(lista_numero)
+
+#Questão 10
+
+linhas = 3
+colunas = 3
+matriz_3x3 = []
+soma_impar = 0
+for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+        valor = int(input(f"Digite os valares das posicoe: ({i},{j}): "))
+        linha += [valor]
+    matriz_3x3 += [linha]
+
+for i in range(linhas):
+    for j in range(colunas):
+        if matriz_3x3[i][j] % 2 != 0:
+            soma_impar += 1
+
+for _ in matriz_3x3:
+    print(_)
+
+print(f"Valores impares na matriz: {soma_impar}")
