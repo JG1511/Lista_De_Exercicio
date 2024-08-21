4423ed2d4cec74a341935f1b33a0442d5a64db15
JOÃO GUILHERME DE LIMA OLIVEIRA <20241148060011@ifrn.local>
2024-08-21 11:35:06 -0300

    atualizacao

--- listaRapetiçao/teste.py
+++ listaRapetiçao/teste.py
@@ -1,17 +1,28 @@
 #Questão 11 lista 03 
 
-quantidade_numeros  =  int(input("Digite a quantidade de numeros: "))
-soma_numeros = 0
-num_str = ""
+# quantidade_numeros  =  int(input("Digite a quantidade de numeros: "))
+# soma_numeros = 0
+# num_str = ""
 
-for i in range(quantidade_numeros ):
-    valor = int(input("Digite seus numero: "))
-    soma_numeros += valor
+# for i in range(quantidade_numeros ):
+#     valor = int(input("Digite seus numero: "))
+#     soma_numeros += valor
 
-    if i == 0:
-        num_str += str(valor)
-    else:
-        num_str += f" + {valor}"
+#     if i == 0:
+#         num_str += str(valor)
+#     else:
+#         num_str += f" + {valor}"
 
-print(f"Soma: {num_str} = {soma_numeros}")
+# print(f"Soma: {num_str} = {soma_numeros}")
 
+
+# AULA 13
+
+tupla_inicial = (1,2,3,4,5)
+tuplas_final = ()
+
+for item in tupla_inicial:
+    if item % 2 == 0:
+        tuplas_final += (item,)
+
+print(tuplas_final)
