d57a0d42467c0768e06b64dcab7ea8c0b33375d2
JOÃO GUILHERME DE LIMA OLIVEIRA <20241148060011@ifrn.local>
2024-08-26 09:58:52 -0300

    terminardps

---
+++ tuplas_dic_set/questao06.py
@@ -0,0 +1,12 @@
+tamanho = int(input("quantos nomes: "))
+lista_nome = []
+
+print("verificador de políndromo")
+
+for i in range(tamanho):
+    nome = input("Digite os nomes: ")
+    lista_nome.append(nome)
+
+print(lista_nome)
+
+# Fazer o polindromo
